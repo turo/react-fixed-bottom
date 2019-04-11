745f730177e69c35b35024a15583837e0645dc25
Esau Suarez <esau@turo.com>
2019-04-11 23:32:11 +0100

    feat(index): adds source code for the FixedBottom component

---
+++ src/util.js
@@ -0,0 +1,21 @@
+/**
+ * This function returns true if we successfully detect Safari mobile.
+ * It should test against Apple devices and then exclude most non Safari browsers
+ * Source in https://stackoverflow.com/questions/3007480/determine-if-user-navigated-from-mobile-safari
+ *
+ * @return {boolean}
+ */
+export function isSafariMobile() {
+  if (typeof window === 'undefined' || typeof window.navigator === 'undefined') {
+    return false;
+  }
+
+  const {
+    navigator: {userAgent},
+  } = window;
+  return (
+    /iP(ad|od|hone)/i.test(userAgent) &&
+    /WebKit/i.test(userAgent) &&
+    !/(CriOS|FxiOS|OPiOS|mercury)/i.test(userAgent)
+  );
+}
