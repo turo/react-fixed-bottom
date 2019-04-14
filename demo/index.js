2058fa96e073e7b390e4a7c01b04b3dbfcc4dc29
Esau Suarez <esau@turo.com>
2019-04-14 13:31:54 +0100

    chore(demo): updates demo to use built lib

--- demo/index.js
+++ demo/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FixedBottom from '../src/index';
+import FixedBottom from '../dist/index';
 
 const App = () => (
-  <div style={{background: '#333' ,height: 5000}}>
+  <div
+    style={{
+      background: 'linear-gradient(#4c4c4c 0%, #000 100%)',
+      height: 5000,
+    }}
+  >
     <FixedBottom offset={20}>
-      <div style={{
-        background: '#000',
-        color: '#fff',
-        height: 60,
-        left: 0,
-        right: 0,
-        textAlign: 'center'
-      }}>
-        I'm a fixed Bar with a 20px offset from the edge of the screen which is actually usable in Safari mobile.
+      <div
+        style={{
+          background: '#000',
+          color: '#fff',
+          height: 60,
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+        }}
+      >
+        I'm a fixed Bar with a 20px offset from the edge of the screen which is actually usable in
+        Safari mobile.
       </div>
     </FixedBottom>
   </div>
